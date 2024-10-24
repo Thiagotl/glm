ff0cbe4e1363fbc48d4a7ee1457841720b23744f
Thiago Tavares <thiago.tlopess@gmail.com>
2024-10-24 13:19:48 -0300

    att

--- atividades.R
+++ atividades.R
@@ -1,1 +1,8 @@
 # Atividade GLM 
+data<-read.table("Dados_aula_4.txt",h=T)
+attach(data)
+fit1<- glm(resp~cov1+cov2+cov3, family=poisson(link="log"))
+summary(fit1)
+
+fit2 <- glm(resp~cov1+cov3, family=poisson(link="log"))
+summary(fit2)
