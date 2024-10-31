4e8bb91483533fb801bb6c87bc4d753f4efbb952
Thiago Tavares <thiago.tlopess@gmail.com>
2024-10-30 21:05:13 -0300

    att

--- trab_final/script.R
+++ trab_final/script.R
@@ -5,6 +5,11 @@ library(tidyverse)
 
 banco <- read_excel("trab_final/Dados gerais das estações 2023.xlsx")
 View(banco)
+attach(banco)
+glimpse(banco)
+banco$pm10<-as.double(banco$pm10)
+banco$co<-as.double(banco$co)
+
 
 banco<-banco |> slice(-1)
 
@@ -14,15 +19,19 @@ banco<-banco |>
 
 cal_media_diaria<-function(df, data_col, vars) {
   banco |> 
-    group_by({{data_col}}) |> 
+    group_by({{ data_col }}) |> 
     summarize(across(all_of(vars), mean, na.rm = TRUE), .groups = "drop")
 }
 
 media_diaria<-cal_media_diaria(banco, data, c("pm10", "so2", "no2", "o3","co"))
 
-View(media_diaria)
+glimpse(banco)
 
 
 
 
+
+# media_teste<- banco |> 
+#   group_by(data) |> 
+#   summarize(varaivel_media = mean(variavel,na.rm = TRUE))
 
