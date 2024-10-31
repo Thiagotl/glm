cead8c4c569998304bc377c7a86a2686ce7a9814
Thiago Tavares <thiago.tlopess@gmail.com>
2024-10-31 16:09:58 -0300

    att

--- trab_final/script.R
+++ trab_final/script.R
@@ -35,3 +35,37 @@ glimpse(banco)
 #   group_by(data) |> 
 #   summarize(varaivel_media = mean(variavel,na.rm = TRUE))
 
+
+# devido a quantidade de NA a variável pm10 será excluida
+media_diaria<-media_diaria |> select(-pm10)
+
+# será removido a linha 58 devido a presença de NA
+
+media_diaria<-media_diaria |> slice(-58)
+
+
+# No caso da variavel o3, há apenas um valor Na, esse será substituido pela média geral
+
+media_diaria$o3[is.na(media_diaria$o3)]<-mean(media_diaria$o3, na.rm = TRUE)
+
+
+# verificar o valores minimos estabelecidos para criar a variável 0 e 1 para qualidade do ar
+
+
+
+limites_conama<-c(so2=40, no2=200, o3=100 , co=9)
+
+
+
+media_diaria<-media_diaria |> 
+  mutate(
+    aceitavel = if_else(
+      rowSums(across(names(limites_conama),~.<=limites_conama[cur_column()])) == length(limites_conama),
+      1,0
+    )
+  )
+
+ # todos os dias estão dentro do limite aceitável - o banco será descartado.
+# https://aqicn.org/city/sao-paulo/pt/
+
+# Your token is f6cd930c4f6acd42e01a1e1bc3ff03a33c913d7b
